20604d615c87c545eccc1262844dc46b175b25bf
Manuel <mlabous@ekylibre.com>
2021-05-14 07:53:11 +0000

    Implementing permanent_grasslands, low_number_animals & sie infos

--- lib/idea.rb
+++ lib/idea.rb
@@ -1,6 +1,19 @@
-require 'idea/engine.rb'
-require 'idea/version.rb'
+# frozen_string_literal: true
 
+require 'idea/engine'
+require 'idea/version'
+
+# Analytics on farm performance towards durability
 module Idea
+  # parse transcodings
+  # @return [OpenStruc] with keys: permanent_grasslands, low_number_animals, ecological_interest_areas
+  def self.transcodings
+    transcoding_path = File.join(File.dirname(__dir__), 'config', 'transcodings.json')
+    JSON.parse(File.read(transcoding_path), object_class: OpenStruct)
+  end
 
+  # @returns [Array] all grasslands pac codes
+  def self.permanent_grassland_codes
+    transcodings.permanent_grassland.map(&:code)
+  end
 end
